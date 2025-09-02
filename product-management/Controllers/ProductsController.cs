e164a0bdcb7bcd691133a1411f4d0df2e56b91d3
apolinario0x21 <imarcelo.apolinario@gmail.com>
2025-09-02 16:52:47 -0300

    add test

---
+++ product-management/Controllers/ProductsController.cs
@@ -0,0 +1,68 @@
+using ProductStore.Models;
+
+public class ProductsController : ApiController
+{
+    static readonly IProductRepository repository = new ProductRepository();
+}
+
+public class ProductsController : ApiController
+{
+    public IEnumerable<Product> GetAllProducts()
+    {
+        return repository.GetAll();
+    }
+    // ....
+}
+
+public Product GetProduct(int id)
+{
+    Product item = repository.Get(id);
+    if (item == null)
+    {
+        throw new HttpResponseException(HttpStatusCode.NotFound); 
+    }
+    return item;
+}
+
+public IEnumerable<Product> GetProductsByCategory(string category)
+{
+    return repository.GetAll().Where(
+        p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+}
+
+// Not the final implementation!
+public Product PostProduct(Product item)
+{
+    item = repository.Add(item);
+    return item;
+}
+
+public HttpResponseMessage PostProduct(Product item)
+{
+    item = repository.Add(item);
+    var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+    string uri = Url.Link("DefaultApi", new { id = item.Id });
+    response.Headers.Location = new Uri(uri);
+    return response;
+}
+
+public void PutProduct(int id, Product product)
+{
+    product.Id = id;
+    if (!repository.Update(product))
+    {
+        throw new HttpResponseException(HttpStatusCode.NotFound);
+    }
+}
+
+public void DeleteProduct(int id)
+{
+    Product item = repository.Get(id);
+    if (item == null)
+    {
+        throw new HttpResponseException(HttpStatusCode.NotFound);
+    }
+
+    repository.Remove(id);
+}
